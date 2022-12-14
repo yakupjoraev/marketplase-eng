e6cb7063816b3cfc2aa66ff3d61dcdddcbbf6987
Yakup <yakupjoraev@gmail.com>
2022-12-14 12:43:57 +0300

    1

---
+++ features/config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C2LTKACST061L8G6SHEG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true});
+window[window["TiktokAnalyticsObject"]].instance("C2LTKACST061L8G6SHEG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C2LTKACST061L8G6SHEG").setPixelInfo({status: 0, name: "CM Events", advertiserID: "6806194978323169285", setupMode: 1, partner: "", is_onsite: false});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("challengermode.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
